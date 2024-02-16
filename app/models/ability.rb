3db11f0465f533d4cef472ab5ed236d87972a64d
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-02-16 18:43:30 +0200

    Define ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,13 @@
 class Ability
   include CanCan::Ability
+  def initialize(user)
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, Recipe, user_id: user.id
+      can :manage, Food, user_id: user.id
+    end
+  end
 end
